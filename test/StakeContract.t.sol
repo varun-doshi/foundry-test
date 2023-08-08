59b505870fb8c55a4a14de8c1ee234572b3da903
Varun Doshi <doshivarun202@gmail.com>
2023-08-09 00:00:01 +0530

    uipdate

---
+++ test/StakeContract.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "../lib/forge-std/src/Test.sol";
+import {Counter} from "../src/Counter.sol";
+
+
+contract StakeContract is Test{
+    
+}
