59f166ff8b9ac2f4acc9159c8d836bf972d96875
Varun Doshi <doshivarun202@gmail.com>
2023-08-09 00:33:26 +0530

    added test

--- test/StakeContract.t.sol
+++ test/StakeContract.t.sol
@@ -2,9 +2,24 @@
 pragma solidity ^0.8.13;
 
 import {Test, console2} from "../lib/forge-std/src/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import "../src/StakeContract.sol";
+import "./mocks/MockERC20.sol";
 
 
-contract StakeContract is Test{
-    
+contract StakeContractTest is Test{
+    StakeContract public stakecontract;
+    MockERC20 public mockerc20;
+    function setUp() public{
+        stakecontract=new StakeContract();
+        mockerc20 =new MockERC20();
+
+    }
+
+    function testExample() public{
+        uint256 amount=10e18;
+        mockerc20.approve(address(stakecontract), amount);
+        bool stakePassed=stakecontract.stake(amount,address(mockerc20));
+        assertTrue(stakePassed);
+    }
+
 }
